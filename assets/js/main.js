43bb133a5545853a7af3255f299680cf9dc6c741
Jerome <jeromelesne@gmail.com>
2024-01-04 02:00:16 +0100

    drop animation implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,9 @@ let array = [
     [0, 0, 0, 0, 0, 0, 0],
 ];
 
+let playerSwitch = true;
+let cellH;
+
 const gameContainer = document.querySelector("#gameContainer");
 
 function displayGrid() {
@@ -23,7 +26,6 @@ function displayGrid() {
             row.appendChild(cell);
             cell.addEventListener("click", () => {
                 pvpPlay(j);
-                displayContent();
             });
         });
     });
@@ -33,23 +35,37 @@ function pvpPlay(j) {
     array.forEach((e, i) => {
         if (i == array.length - 1) {
             e[j] = 1;
+            displayContent(i);
         } else if (array[i + 1][j] != 0) {
             e[j] = 1;
+            displayContent(i);
         }
     });
 }
 
-function displayContent() {
+function displayContent(indexH) {
     array.forEach((e, i) => {
         i++;
         const row = document.querySelector(`#gridContainer > :nth-child(${i})`);
         e.forEach((el, j) => {
             j++;
             const cell = row.querySelector(`:nth-child(${j})`);
-            if (el == 1) {
-                cell.innerHTML = "";
+            if (el == 1 && cell.innerHTML == "") {
+                const dropHeight = i * cell.clientHeight;
+                const dropTime = dropHeight / 1;
                 const img = document.createElement("img");
                 img.src = "./assets/images/red-token.png";
+                img.style.width = "63px";
+                img.style.transform = `translateY(-${dropHeight}px)`;
+                setTimeout(() => {
+                    img.style.transition = `transform ${dropTime}ms ease-in`;
+                    img.style.transform = "translateY(0)";
+                }, 50);
+                cell.appendChild(img);
+            } else if (el == 2) {
+                cell.innerHTML = "";
+                const img = document.createElement("img");
+                img.src = "./assets/images/yellow-token.png";
                 img.style.width = "63px";
                 cell.appendChild(img);
             }
